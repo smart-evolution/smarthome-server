3b3c0247bff2a8a9a2bf2801347625ae99b17f20
Oskar Szura <oskar.szura@gmail.com>
2019-08-21 17:14:09 +0000

    HOME-224 - Replace sleep with while in smarthome-server scripts where possible

--- start_services.sh
+++ start_services.sh
@@ -1,3 +1,4 @@
+echo "Starting services ..."
 mongod --repair > /dev/null 2>&1 &&
 mongod > /dev/null 2>&1 &
 influxd > /dev/null 2>&1 &
@@ -6,3 +7,5 @@ while ! curl -sl -I http://localhost:8086/ping
 do
   sleep 1
 done
+
+echo "Influxdb is running"
