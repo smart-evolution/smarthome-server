01da13bb0475118e2b7bb6a22aef97fad2edc72f
Oskar Szura <oskar.szura@gmail.com>
2019-12-03 15:01:49 +0100

    HOME-259 - Add installation scripts to Debian

--- debian/install-golang.sh
+++ debian/install-golang.sh
@@ -2,7 +2,7 @@ wget https://dl.google.com/go/go1.12.7.linux-amd64.tar.gz && \
 tar -C /usr/local -xvf go1.12.7.linux-amd64.tar.gz && \
 {
     echo ''
-    echo '#Go ENV variables'
+    echo '# Go ENV variables'
     echo 'PATH="${PATH}:/usr/local/go/bin"'
     echo 'GOPATH=~/gowork'
     echo 'GO111MODULE=on'
